dbd950098d5f66112ec322be702b166b8e661c56
bisola-agbede <bisolaagbede01@gmail.com>
2022-08-08 18:44:06 -0700

    file that handles error message for env in get_env

--- handle_error_2.c
+++ handle_error_2.c
@@ -37,7 +37,7 @@ char *error_env(data_shell *datash)
 }
 /**
  * error_path_126 - error message for path and failure denied permission.
- * @datash: data relevant (counter, arguments).
+ * @datash: Data relevant (counter, arguments).
  *
  * Return: The error string.
  */
